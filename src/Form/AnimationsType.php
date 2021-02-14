cad33d264dc47cfbb98166201c0b48bbcce88d34
Tar3ntino <mynewmailkantin@hotmail.fr>
2021-02-15 00:51:43 +0100

    installation des bundle CKEditor et Elfinder pour l'edition de texte back office et chargement d'image

--- src/Form/AnimationsType.php
+++ src/Form/AnimationsType.php
@@ -4,13 +4,14 @@ namespace App\Form;
 
 use App\Entity\Animations;
 use App\Entity\Categories;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
  
 class AnimationsType extends AbstractType
 {
@@ -18,7 +19,7 @@ class AnimationsType extends AbstractType
     {
         $builder
             ->add('title', TextType::class)
-            ->add('description', TextareaType::class)
+            ->add('description', CKEditorType::class)
             ->add('categories', EntityType::class,array(
                 'class' => Categories::class,
                 'multiple' => true // nécessaire lorsque l'on a une relation many to many, car l'on précise que l'on peut accepter plusieurs catégories dans une variable tableau rattaché à une meme animation
