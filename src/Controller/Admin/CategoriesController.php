7adac1048698402df5bd9982c5b35ec4cc6fd3c3
Tar3ntino <mynewmailkantin@hotmail.fr>
2021-02-20 00:46:01 +0100

    Ajout et Modification ADMIN de l'entité Categorie

--- src/Controller/Admin/CategoriesController.php
+++ src/Controller/Admin/CategoriesController.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Animations;
 use App\Entity\Categories;
 use App\Form\AnimationsType;
 use App\Form\CategoriesType;
+use App\Repository\CategoriesRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,23 +14,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 /**
- * @Route("/admin", name="admin_")
- * @package App\Controller
+ * @Route("/admin/categories", name="admin_categories_")
+ * @package App\Controller\Admin
  */
-class AdminController extends AbstractController
+class CategoriesController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(CategoriesRepository $catsrepo)
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        return $this->render('admin/categories/index.html.twig', [
+            'categories' => $catsrepo->findAll()
         ]);
     }
 
     /**
-     * @Route("/categories/ajout", name="categories_ajout")
+     * @Route("/ajout", name="ajout")
      * Pour créer un formulaire, il est nécessaire d'avoir en paramètre l'objet Request
      * provenant de la classe HttpFoundation à importer use...
      */
@@ -53,7 +54,7 @@ class AdminController extends AbstractController
             $em->persist($categorie);
             $em->flush();
 
-            return $this->redirectToRoute('admin_home');
+            return $this->redirectToRoute('admin_categories_home');
 
         }
 
@@ -61,6 +62,40 @@ class AdminController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+/**
+     * @Route("/modifier/{id}", name="modifier")
+     * Pour créer un formulaire, il est nécessaire d'avoir en paramètre l'objet Request
+     * provenant de la classe HttpFoundation à importer use...
+     */
+    public function modifCategorie(Categories $categorie, Request $request)
+    {
+        /* Creation d'un formulaire pour pouvoir ajouter la nouvelle catégorie que l'on va renvoyer dans la vue pour la saisie : */
+        $form = $this->createForm(CategoriesType::class, $categorie);
+
+        /* Traitement de la request du formulaire une fois le bouton 'valider' cliqué : */
+
+        $form->handleRequest($request);
+
+        /* Dans le cas ou le formulaire est soumis ET valide : */
+        if ($form->isSubmitted() && $form->isValid()){
+
+            /* Entity manager = em */
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($categorie);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_categories_home');
+
+        }
+
+        return $this->render('admin/categories/ajout.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+
 
     /**
      * @Route("/animations/ajout", name="animations_ajout")
